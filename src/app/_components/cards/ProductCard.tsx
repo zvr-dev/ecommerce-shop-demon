1631e99012fab0d2b84dac22b0bbbb71f8753172
zvr-dev <zavier.the@hotmail.com>
2025-07-22 23:55:35 +1000

    implemented ratings to product card and added more dummyData

--- src/app/_components/cards/ProductCard.tsx
+++ src/app/_components/cards/ProductCard.tsx
@@ -14,7 +14,7 @@ export type ProductCardProps = {
     imgUrl: string;
     productUrl: string;
 }
-export const ProductCard = ({id, name, fit, color, tag, price, imgUrl, productUrl}: ProductCardProps) => {
+export const ProductCard = ({id, name,rating, fit, color, tag, price, imgUrl, productUrl}: ProductCardProps) => {
 
     return <li role="list">
         <article className="product-card">
@@ -39,6 +39,11 @@ export const ProductCard = ({id, name, fit, color, tag, price, imgUrl, productUr
             </div>
             <Link href={productUrl}>
             <div className='product-details-wrapper'>
+                {rating? <span className="product-rating">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" >
+                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                    </svg> {rating}
+                </span> : ""}
                 <p className="product-name">{name}</p>
                 {fit ? <p className="product-fit">{fit}</p> : ""}
                 <p className="product-color">{color}</p>
--- src/app/_components/scrollers/ProductScroller.tsx
+++ src/app/_components/scrollers/ProductScroller.tsx
@@ -10,59 +10,111 @@ type ProductScrollerProps = {
 
 export const ProductScroller = ({title, data, sectionUrl}: ProductScrollerProps) => {
     let dummyData = [
-        {
-            id: 1,
-            name: "item1",
-            fit: "overside fit",
-            color: "black",
-            tag: "new",
-            price: 55,
-            imgUrl: "/img/home/hero2.jpg",
-            productUrl: "/product/"
-        },
-        {
-            id: 2,
-            name: "item1",
-            rating:4,
-            fit: "overside fit",
-            color: "black",
-            tag: "hot",
-            price: 33,
-            imgUrl: "/img/home/hero2.jpg",
-            productUrl: "/product/"
-        },
-        {
-            id: 3,
-            name: "item1",
-            rating:5,
-            fit: "overside fit",
-            color: "black",
-            price: 55,
-            imgUrl: "/img/home/hero2.jpg",
-            productUrl: "/product/"
-        },
-        {
-            id: 4,
-            name: "item1",
-            rating:5,
-            fit: "overside fit",
-            color: "black",
-            tag: "new",
-            price: 35,
-            imgUrl: "/img/home/hero2.jpg",
-            productUrl: "/product/"
-        },
-        {
-            id: 5,
-            name: "item1",
-            fit: "overside fit",
-            color: "black",
-            tag: "new",
-            price: 28,
-            imgUrl: "/img/home/hero2.jpg",
-            productUrl: "/product/"
-        },
-    ]
+  {
+    id: 1,
+    name: "Varsity Mesh Shorts",
+    fit: "regular fit",
+    color: "black",
+    tag: "new",
+    price: 55,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 2,
+    name: "Varsity Oversized Shirt",
+    rating: 4.2,
+    fit: "oversized fit",
+    color: "black",
+    tag: "hot",
+    price: 33,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 3,
+    name: "Varsity 7\" Shorts",
+    rating: 5,
+    fit: "slim fit",
+    color: "black",
+    price: 55,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 4,
+    name: "Oversized Joggers",
+    rating: 5,
+    fit: "oversized fit",
+    color: "black",
+    tag: "new",
+    price: 35,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 5,
+    name: "Tech T-Shirt",
+    fit: "regular fit",
+    color: "black",
+    tag: "new",
+    price: 28,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 6,
+    name: "Athletic Hoodie",
+    rating: 4.7,
+    fit: "loose fit",
+    color: "charcoal",
+    tag: "hot",
+    price: 60,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 7,
+    name: "Performance Tank",
+    fit: "slim fit",
+    color: "white",
+    price: 25,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 8,
+    name: "Lightweight Zip Hoodie",
+    rating: 4.5,
+    fit: "regular fit",
+    color: "grey",
+    tag: "hot",
+    price: 42,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 9,
+    name: "Essential Sweatpants",
+    fit: "regular fit",
+    color: "black",
+    tag: "new",
+    price: 38,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  },
+  {
+    id: 10,
+    name: "Mesh Training Shorts",
+    rating: 4.8,
+    fit: "athletic fit",
+    color: "navy",
+    tag: "hot",
+    price: 32,
+    imgUrl: "/img/home/hero2.jpg",
+    productUrl: "/product/"
+  }
+];
 
     function normalizeTag(input:string | undefined): Tag {
         return input ? input.toLowerCase() as Tag : undefined; 
