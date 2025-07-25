736637633f0b58962409bfead87218a5fd5eb73d
zvr-dev <zavier.the@hotmail.com>
2025-07-26 03:01:43 +1000

    modified bg colour, refactored code

--- src/app/_components/buttons/customButton.tsx
+++ src/app/_components/buttons/customButton.tsx
@@ -2,9 +2,10 @@ import './customButton.css'
 type ButtonProps = {
     variant: "light" | "dark";
     children: React.ReactNode;
+    onClick?: () => void;
 }
-export const CustomButton = ({variant, children}: ButtonProps) => {
-    return <button className={`btn ${variant}`}>
+export const CustomButton = ({variant, children, onClick}: ButtonProps) => {
+    return <button className={`btn ${variant}`} onClick={onClick}>
         {children}
     </button>
 }
