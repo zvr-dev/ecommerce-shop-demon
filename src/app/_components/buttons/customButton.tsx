2ef3ea55f2dcf7bf6930b27d9ce95606cfa936c4
zvr-dev <zavier.the@hotmail.com>
2025-07-27 19:30:42 +1000

    adjused to allow changing font sizes/button/size

--- src/app/_components/buttons/customButton.tsx
+++ src/app/_components/buttons/customButton.tsx
@@ -2,10 +2,13 @@ import './customButton.css'
 type ButtonProps = {
     variant: "light" | "dark";
     children: React.ReactNode;
+    fontSize?: string;
     onClick?: () => void;
 }
-export const CustomButton = ({variant, children, onClick}: ButtonProps) => {
-    return <button className={`btn ${variant}`} onClick={onClick}>
+export const CustomButton = ({variant, children, onClick, fontSize}: ButtonProps) => {
+    return <button className={`btn ${variant}`} 
+        style={{fontSize:fontSize}} 
+        onClick={onClick}>
         {children}
     </button>
 }
