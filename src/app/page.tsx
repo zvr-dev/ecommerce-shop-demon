e413c0bda8bacb8e5610ff60ef71394919daf4a4
zvr-dev <zavier.the@hotmail.com>
2025-07-27 19:31:12 +1000

    progress on implement video player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
-import styles from './page.module.css'
+import style from './page.module.css'
 import { CustomButton } from './_components/buttons/customButton';
 import Link from 'next/link';
 import { ProductCarousel } from './_components/carousels/ProductCarousel';
 import { NavigationCarousel } from './_components/carousels/NavigationCarousel';
 
 import { dummyCategoryCards, dummyNavCardArray } from '@/app/dummydata';
+import { VideoPlayer } from './_components/videoPlayers/VideoPlayer';
 
 export default function Home() {
 
   return (
-    <div id='hero' className={styles.content}>
+    <div id='hero' className={style.content}>
       {/* HERO SECTION */}
-      <section  className={styles.hero_block}>
-        <img src="/img/home/hero.avif" alt="VARSITY. BEST IN CLASS." className={styles.hero_img}/>
-        <div className={styles.hero_content}>
-          <h1>Varsity. Best In Class.</h1>
+      <section  className={style.hero_block}>
+        <img src="/img/home/hero.avif" alt="new drops for your next workout" className={style.hero_img}/>
+        <div className={style.hero_content}>
+          <h1>new drops for your next workout</h1>
           <p>Strong enough for gym. Comfy enough for chilling. Versatile enough for everyday.</p>
-          <div className={styles.hero_block_button_group}>
+          <div className={style.hero_block_button_group}>
             <a href="/womens"><CustomButton variant="light">Shop Women's</CustomButton></a>
             <a href="/mens"><CustomButton variant="light">Shop Men's</CustomButton></a>
             </div>
@@ -24,21 +25,24 @@ export default function Home() {
       </section>
 
       {/* Social Proof for new campaign */}
-      <ProductCarousel title="Get The Look" sectionUrl='/'/>
+      {/* <ProductCarousel title="Get The Look" sectionUrl='/'/> */}
 
       {/* New Campaign Items */}
-      <ProductCarousel title="Varsity. Best In Class"  sectionUrl='/'/>
+      <ProductCarousel title="new drops for your next workout"  sectionUrl='/'/>
 
 
       {/* Popular Items */}
       <section>
         <NavigationCarousel title="popular right now" cardArray={dummyNavCardArray}/>
       </section>
-      <div className={styles.video_wrapper}>
-        <video autoPlay={true} muted playsInline width={"100%"} loop>
-          <source type='video/mp4' src="/vid/July_Seasonal_Together_Look_1_Web_Banner_8x3.mp4"/>
-        </video>
-      </div>
+      
+      {/* Video */}
+      <VideoPlayer 
+        src="/vid/July_Seasonal_Together_Look_1_Web_Banner_8x3.mp4" 
+        title="Strong Season"
+        content="Bold shades and sick patterns for you to lift heavy, run fast and push hard in."
+
+        />
 
       {/* New Items */}
       <section>
