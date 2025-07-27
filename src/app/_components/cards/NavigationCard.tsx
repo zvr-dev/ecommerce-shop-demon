5ad62863d63654c736eb6429c277c03eedd77877
zvr-dev <zavier.the@hotmail.com>
2025-07-27 23:18:45 +1000

    refactored code

--- src/app/_components/cards/NavigationCard.tsx
+++ src/app/_components/cards/NavigationCard.tsx
@@ -1,5 +1,5 @@
 import { GenderTypes } from "@/app/_types/types"
-import "./NavigationCard.css"
+import style from "./NavigationCard.module.css"
 import Link from "next/link"
 import { CustomButton } from "../buttons/customButton"
 export type NavigationCardProps = {
@@ -13,25 +13,24 @@ export type NavigationCardProps = {
 
 export const NavigationCard = ({title, description, buttonLabel, imgUrl, url} : NavigationCardProps) => {
 
-    return <li role="list">
-            <article className="nav-card-wrapper">
-                <div>
-
+    return <li>
+        <article className={style.card_wrapper}>
+            <div>
                 <Link href={url}>
-                    <div className="nav-card-img-wrapper">
-                        <img className="nav-card-img" src={imgUrl} alt={title} loading="lazy"/>
+                    <div className={style.card_img_wrapper}>
+                        <img className={style.card_img} src={imgUrl} alt={title} loading="lazy"/>
                     </div>
                 </Link>
             {title? 
-                <h3 className="nav-card-title">{title}</h3>:""}
+                <h3 className={style.card_title}>{title}</h3>:""}
             {description?
-                <p className="nav-card-description">{description}</p>:""}
+                <p className={style.card_description}>{description}</p>:""}
             </div>
-            <div className="nav-card-btn-wrapper">
+            <div className={style.card_btn_wrapper}>
                 <Link href={url}>
                     <CustomButton variant="dark">{buttonLabel}</CustomButton>
                 </Link>
             </div>
-    </article>
+        </article>
     </li>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,22 @@ import Link from 'next/link';
 import { CustomButton } from './_components/buttons/customButton';
 import { ProductCarousel } from './_components/carousels/ProductCarousel';
 import { NavigationCarousel } from './_components/carousels/NavigationCarousel';
-import { VideoHero } from './_components/videoPlayers/VideoHero';
+import { VideoHero } from './_components/heros/VideoHero';
 
 import style from './page.module.css'
 import { dummyCategoryCards, dummyNavCardArray } from '@/app/dummydata';
+import { Hero } from './_components/heros/Hero';
 
 export default function Home() {
 
   return (
     <div id='hero' className={style.content}>
       {/* HERO SECTION */}
-      <section  className={style.hero_block}>
-        <img src="/img/home/hero.avif" alt="new drops for your next workout" className={style.hero_img}/>
-        <div className={style.hero_content}>
-          <h1>new drops for your next workout</h1>
-          <p>Strong enough for gym. Comfy enough for chilling. Versatile enough for everyday.</p>
-          <div className={style.hero_block_button_group}>
-            <a href="/womens"><CustomButton variant="light">Shop Women's</CustomButton></a>
-            <a href="/mens"><CustomButton variant="light">Shop Men's</CustomButton></a>
-            </div>
-        </div>
-      </section>
+      <Hero 
+        title = "new drops for your next workout."
+        content = "Strong enough for gym. Comfy enough for chilling. Versatile enough for everyday."
+        imgSrc = {"/img/home/hero.avif"}
+      />
 
       {/* Social Proof for new campaign */}
       {/* <ProductCarousel title="Get The Look" sectionUrl='/'/> */}
@@ -56,18 +51,14 @@ export default function Home() {
           cardArray={dummyNavCardArray.map(({description, ...rest}) => ({...rest}))}/>
       </section>
 
-      {/* WOMEN MEN ACCESSPROES */}
+      {/* WOMEN MEN ACCESSORIES */}
       <section>
         <NavigationCarousel cardArray={dummyCategoryCards}
         />
       </section>
-
-      {/* QUICK LINKS */}
-      <section>
-
-      </section>
-
-      <Link href="/#">Back to top</Link>
+      <div className={style.btt_wrapper}>
+        <Link href="/#"><button className={style.back_to_top_btn}>Back to top</button></Link>
+      </div>
     </div>
   );
 }
