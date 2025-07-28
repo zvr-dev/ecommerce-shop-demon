da7b587206795c5e75b38e38e3ac3e9ad42a1ab6
zvr-dev <zavier.the@hotmail.com>
2025-07-28 21:32:21 +1000

    added ButtonLink type

--- src/app/_types/types.tsx
+++ src/app/_types/types.tsx
@@ -1,2 +1,7 @@
 export type GenderTypes = "men" | "women" | "accessories";
 export type ProductTypes = "t-shirts" | "pants" | "trending"
+
+export type ButtonLink = {
+    content: string;
+    url: string;
+}
