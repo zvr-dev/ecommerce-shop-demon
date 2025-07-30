c09086cb7c3728088c473635027d9329b7ca5fe1
zvr-dev <zavier.the@hotmail.com>
2025-07-30 23:52:23 +1000

    styled 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,23 @@
 import { CustomButton } from "./_components/buttons/custom-button";
-
+import style from "./not-found.module.css";
 export default function NotFound() {
   return (
-    <div className="">
-      <h1>404</h1>
-      <h2>Not Found</h2>
-      <p>Not sure whats happened there. Maybe go grab a new fit instead?</p>
-      <div className="">
-        <CustomButton variant="light">Shop Women's</CustomButton>
-        <CustomButton variant="light">Shop Men's</CustomButton>
+    <main className={style.wrapper}>
+      <div className={style.content}>
+        <div>
+          <h1>404</h1>
+          <h2>Not Found</h2>
+        </div>
+        <p>Not sure whats happened there. Maybe go grab a new fit instead?</p>
+        <div className={style.buttons}>
+          <CustomButton href="/women" variant="light">
+            Shop Women's
+          </CustomButton>
+          <CustomButton href="/men" variant="light">
+            Shop Men's
+          </CustomButton>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
